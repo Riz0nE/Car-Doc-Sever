f3b1a7ada0330743eda943f0cc522c79ba1b2014
Riz0nE <108361584+Riz0nE@users.noreply.github.com>
2024-05-21 16:43:29 +0600

    jwt-starting

---
+++ __jwt__steps__.js
@@ -0,0 +1,19 @@
+/* 
+ *install jsonwebtoken
+ *jwt.sign(paylode,secret,{expiresIn:})
+ * token client
+
+*/
+
+/*
+* 1.set cookies with http only,for development secure: false,
+
+* 2.cors 
+
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials:true
+})); 
+
+* 3.client side axios setting
+*/ 
